69a8c44e36b3aaf5dbc89b1b4b47502b7e9f03db
Jose Zothner Meyer <zothner.meyer.jose@gmail.com>
2025-02-18 12:11:04 +0200

    App folder created.

--- api/api_template.py
+++ api/api_template.py
@@ -14,54 +14,28 @@ from pydantic import BaseModel
 # TODO                                                       #
 # Import your image processing script here                   #
 import image_processor
+# This script is purely to apply transforms onto raw images!!
 ##############################################################
 
-class FeatureExtractor(nn.Module):
-    def __init__(self,
-                 decoder: dict = None):
-        super(FeatureExtractor, self).__init__()
-
-##############################################################
-# TODO                                                       #
-# Populate the __init__ method, so that it contains the same #
-# structure as the model you used to train the image model   #
-##############################################################
-        
-        self.decoder = decoder
-
-    def forward(self, image):
-        x = self.main(image)
-        return x
-
-    def predict(self, image):
-        with torch.no_grad():
-            x = self.forward(image)
-            return x
-
-# Don't change this, it will be useful for one of the methods in the API
-class TextItem(BaseModel):
-    text: str
-
-
+# COPY PASTE YOUR FineTunedResNet class here!!!!
 
 try:
 #################################################################
 # TODO                                                          #
-# Load the Feature Extraction model. Above, we have initialized #
-# a class that inherits from nn.Module, and has the same        #
-# structure as the model that you used for training it. Load    #
-# the weights in it here.                                       #
+# 1. Initialise the FineTunedResNet model
+# 2. load in the weights    
+# 3. Convert to the FeatureExtractor using that 1 line    #                         
+# Do not make a new class just for the FeatureExtractor         #
 #################################################################
     pass
 except:
-    raise OSError("No Feature Extraction model found. Check that you have the decoder and the model in the correct location")
+    raise OSError("Error in creating Feature Extractor")
 
 try:
 ##################################################################
 # TODO                                                           #
-# Load the FAISS model. Use this space to load the FAISS model   #
-# which is was saved as a pickle with all the image embeddings   #
-# fit into it.                                                   #
+# 1. Load faiss index.pkl
+# 2. Load image embedding ids.pkl
 ##################################################################
     pass
 except:
@@ -76,37 +50,17 @@ def healthcheck():
   msg = "API is up and running!"
   
   return {"message": msg}
-
-  
-@app.post('/predict/feature_embedding')
-def predict_image(image: UploadFile = File(...)):
-    pil_image = Image.open(image.file)
-
-    ################################################################
-    # TODO                                                         #
-    # Process the input and use it as input for the feature        #
-    # extraction model image. File is the image that the user      #
-    # sent to your API. Apply the corresponding methods to extract #
-    # the image features/embeddings.                               #
-    ################################################################
-
-    return JSONResponse(content={
-    "features": "", # Return the image embeddings here
-    
-        })
   
 @app.post('/predict/similar_images')
-def predict_combined(image: UploadFile = File(...), text: str = Form(...)):
-    print(text)
+def predict_combined(image: UploadFile = File(...)):
     pil_image = Image.open(image.file)
     
     #####################################################################
     # TODO                                                              #
-    # Process the input  and use it as input for the feature            #
-    # extraction model.File is the image that the user sent to your API #   
-    # Once you have feature embeddings from the model, use that to get  # 
-    # similar images by passing the feature embeddings into FAISS       #
-    # model. This will give you index of similar images.                #            
+    # 1. Take a raw image
+    # 2. apply necessary transforms onto it
+    # 3. Put transformed image through the FeatureExtractor. That will get you the embeddings.
+    # 4. Use these embeddings with your faiss code to get the similar images          
     #####################################################################
 
     return JSONResponse(content={
